dcf0ddc44c76a98ac3d573b61d1e525ee1770d4f
Robert Chanphakeo <robertlives@gmail.com>
2018-11-30 22:50:34 +1100

    Added missing reference

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
